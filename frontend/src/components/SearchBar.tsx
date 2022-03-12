aa7c6a760d568eca50b9d0b9cd2625378edbdaea
Youguang-Zhou <youguang.zhou@outlook.com>
2022-03-12 20:11:28 +0000

    feat(frontend): add error handling

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { Alert, CircularProgress, Snackbar, Stack } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import { getSearchResults } from '../utils/api'
@@ -9,13 +10,20 @@ interface SearchBarProps {
 const SearchBar = ({ onResultsRetrieved }: SearchBarProps) => {
 	const [input, setInput] = useState<string | undefined>()
 	const [query, setQuery] = useState<string | undefined>()
-	const { data: results } = useQuery(['search', query], () => getSearchResults(query), {
+	const [showErrorMsg, setShowErrorMsg] = useState(false)
+	const {
+		data: results,
+		isError,
+		isLoading,
+	} = useQuery(['search', query], () => getSearchResults(query), {
+		retry: false,
 		enabled: !!query,
 	})
 
 	useEffect(() => {
+		isError && setShowErrorMsg(true)
 		results && onResultsRetrieved(results)
-	}, [results])
+	}, [results, isError])
 
 	return (
 		<div className="w-3/4 text-right md:w-1/2">
@@ -26,9 +34,29 @@ const SearchBar = ({ onResultsRetrieved }: SearchBarProps) => {
 					onKeyDown={({ key }) => key === 'Enter' && setQuery(input)}
 					onChange={({ target: { value } }) => setInput(value)}
 				/>
-				<button onClick={() => setQuery(input)}>🔍</button>
+				<button className="flex items-center" onClick={() => setQuery(input)}>
+					{isLoading ? (
+						<CircularProgress
+							className="text-gray-500 cursor-not-allowed"
+							style={{ width: '20px', height: '20px' }}
+							color="inherit"
+						/>
+					) : (
+						<span>🔍</span>
+					)}
+				</button>
 			</div>
 			{results && <small className="p-1 text-gray-500">{`${results.time} seconds`}</small>}
+			<Snackbar
+				open={showErrorMsg}
+				autoHideDuration={3000}
+				onClose={() => setShowErrorMsg(false)}
+				anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+			>
+				<Stack sx={{ width: '100%' }} spacing={2}>
+					<Alert severity="error">Error happened!</Alert>
+				</Stack>
+			</Snackbar>
 		</div>
 	)
 }
