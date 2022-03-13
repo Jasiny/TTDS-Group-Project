c409fbdc81f74792d39b241876ec709659e29e62
Youguang-Zhou <youguang.zhou@outlook.com>
2022-03-13 16:24:12 +0000

    refactor(backend): refactor code

--- backend/server/controller.py
+++ backend/server/controller.py
@@ -1,30 +1,44 @@
+import json
 import os
 import pickle
+import re
+import sys
+from collections import Counter
+from pathlib import Path
 
+import numpy as np
 from django.conf import settings
 from django.http import JsonResponse
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from scipy.sparse import csr_matrix
 from sentence_transformers import SentenceTransformer
 from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import normalize
 
 from server.utils import get_res_time
 
-with open(os.path.join(settings.BASE_DIR, 'models/data.pkl'), 'rb') as f:
-    data = pickle.load(f)
+# load data
+data_dict = {}
+with open(os.path.join(settings.BASE_DIR, 'data/data_5d.json'), 'r') as f:
+    for i in json.load(f):
+        data_dict[i['word']] = set(i['definitions'])
 
-# embeddings and vectors
-X_embeddings = data['X_embeddings']
-y = data['y']
-# neural model
-model = SentenceTransformer('all-mpnet-base-v2')
-# knn
-knn = NearestNeighbors(metric='cosine')
-knn.fit(X_embeddings)
+
+# load traditional model
+with open(os.path.join(settings.BASE_DIR, 'models/traditional.pkl'), 'rb') as f:
+    traditional = pickle.load(f)
+
+traditional_model = traditional['model']
+traditional_knn = traditional['knn']
+traditional_y = traditional['y']
 
 
 def search(query, knn, y, n=100):
-    dist, nbrs = knn.kneighbors([query], n_neighbors=n)
+    dist, nbrs = knn.kneighbors(query, n_neighbors=n)
     words = [{
         'word': y[i],
+        'defi': list(data_dict[y[i]]) if y[i] in data_dict else ['Definition Not Found.'],
         'score':f'{score:.3f}'
     } for i, score in zip(nbrs[0], 1-dist[0])]
     return words
@@ -34,7 +48,11 @@ def handle_request(request):
     query = request.GET['query']
     sent_time = request.GET['sentTime']
 
-    words = search(model.encode(query), knn, y)
+    query_vec = traditional_model.transform(query)
+    knn = traditional_knn
+    y = traditional_y
+
+    words = search(query_vec, knn, y)
 
     res = {
         'words': words,
