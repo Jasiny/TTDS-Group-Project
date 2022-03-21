9e70ac307857f0f245935a40a70a9974d6dc6a47
Youguang-Zhou <youguang.zhou@outlook.com>
2022-03-21 00:09:20 +0000

    style(frontend): improve style

--- frontend/src/components/DefinitionCard.tsx
+++ frontend/src/components/DefinitionCard.tsx
@@ -10,50 +10,60 @@ interface DefinitionCardProps {
 	onFeedbackBtnClicked: () => void
 }
 
-const DefinitionCard = ({ word, pos, defitions, onFeedbackBtnClicked }: DefinitionCardProps) => (
-	<section className="px-4 py-2 space-y-1 text-left border rounded shadow">
-		<div className="text-2xl text-black capitalize">{word.replaceAll('_', ' ')}</div>
-		<div className="space-x-1 text-xl text-black">
-			{pos.map((p, i) => (
-				<Chip key={i} label={p} size="small" variant="outlined" />
-			))}
-		</div>
-		<div className="overflow-y-auto text-gray-600 max-h-96">
-			{defitions
-				.filter(
-					(d) =>
-						defitions.length == 1 ||
-						(d.split(' ').length > 1 && d.split(' ').length <= 15)
-				)
-				.map((d, i) => (
-					<p key={i} className="text-sm">
-						{`${i + 1}. ${d.charAt(0).toUpperCase() + d.slice(1).toLowerCase() + '.'}`}
-					</p>
+const DefinitionCard = ({ word, pos, defitions, onFeedbackBtnClicked }: DefinitionCardProps) => {
+	const printDefinition = (i: number, d: string) => {
+		d = d.charAt(0).toUpperCase() + d.slice(1).toLowerCase()
+		if (!d.endsWith('.')) {
+			d = d + '.'
+		}
+		return `${i + 1}. ${d}`
+	}
+
+	return (
+		<section className="px-4 py-2 space-y-1 text-left border rounded shadow">
+			<div className="text-2xl text-black capitalize">{word.replaceAll('_', ' ')}</div>
+			<div className="space-x-1 text-xl text-black">
+				{pos.map((p, i) => (
+					<Chip key={i} label={p} size="small" variant="outlined" />
 				))}
-		</div>
-		<div className="flex justify-end">
-			<ButtonGroup size="large">
-				<IconButton
-					style={{ color: 'green' }}
-					onClick={() => {
-						postFeedback(word, 1)
-						onFeedbackBtnClicked()
-					}}
-				>
-					<SentimentSatisfiedAlt />
-				</IconButton>
-				<IconButton
-					style={{ color: 'red' }}
-					onClick={() => {
-						postFeedback(word, -1)
-						onFeedbackBtnClicked()
-					}}
-				>
-					<SentimentVeryDissatisfied />
-				</IconButton>
-			</ButtonGroup>
-		</div>
-	</section>
-)
+			</div>
+			<div className="overflow-y-auto text-gray-600 max-h-96">
+				{defitions
+					.filter(
+						(d) =>
+							defitions.length == 1 ||
+							(d.split(' ').length > 1 && d.split(' ').length <= 15)
+					)
+					.map((d, i) => (
+						<p key={i} className="text-sm">
+							{printDefinition(i, d)}
+						</p>
+					))}
+			</div>
+			<div className="flex justify-end">
+				<ButtonGroup size="large">
+					<IconButton
+						style={{ color: 'green' }}
+						onClick={() => {
+							postFeedback(word, 1)
+							onFeedbackBtnClicked()
+						}}
+					>
+						<SentimentSatisfiedAlt />
+					</IconButton>
+					<IconButton
+						style={{ color: 'red' }}
+						onClick={() => {
+							postFeedback(word, -1)
+							onFeedbackBtnClicked()
+						}}
+					>
+						<SentimentVeryDissatisfied />
+					</IconButton>
+				</ButtonGroup>
+			</div>
+		</section>
+	)
+}
 
 export default DefinitionCard
